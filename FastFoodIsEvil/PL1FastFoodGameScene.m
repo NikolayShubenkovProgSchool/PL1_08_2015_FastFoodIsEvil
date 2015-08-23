442189a491859fffdd9fa8ba542894accded559d
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-08-23 12:38:15 +0300

    block are beeing supressed

--- FastFoodIsEvil/PL1FastFoodGameScene.m
+++ FastFoodIsEvil/PL1FastFoodGameScene.m
@@ -10,15 +10,18 @@
 
 typedef NS_ENUM(NSInteger, PL1FastFoodGameState) {
     PL1FastFoodGameStateInitial,
-    PL1FastFoodGameStateReadyToThrow
+    PL1FastFoodGameStateReadyToThrow,
+    PL1FastFoodGameStateDragging,
+    PL1FastFoodGameStateThrowing
 };
 
 @interface PL1FastFoodGameScene ()
 
 @property (nonatomic) BOOL didInitContent;
 @property (nonatomic) PL1FastFoodGameState state;
-@property (nonatomic, weak) SKPhysicsBody *bodyToThrow;
+@property (nonatomic, strong) SKPhysicsBody *bodyToThrow;
 @property (nonatomic, weak) SKNode *nodeToThrow;
+@property (nonatomic) CGPoint startDragPosition;
 
 @end
 
@@ -28,6 +31,7 @@ typedef NS_ENUM(NSInteger, PL1FastFoodGameState) {
 
 - (void)didMoveToView:(SKView *)view
 {
+    [super didMoveToView:view];
     if (self.didInitContent == NO){
         [self initContent];
     }
@@ -35,8 +39,17 @@ typedef NS_ENUM(NSInteger, PL1FastFoodGameState) {
 
 - (void)initContent
 {
-    SKPhysicsBody *border = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-    self.physicsBody = border;
+
+    SKSpriteNode *aNode = (SKSpriteNode *) [self childNodeWithName:@"background"];
+    
+    SKPhysicsBody *border = [SKPhysicsBody bodyWithEdgeLoopFromRect:CGRectMake(0, 0, aNode.size.width, aNode.size.height)];
+    border.friction       = 0;
+    border.linearDamping  = 0;
+    border.angularDamping = 0;
+    border.restitution    = 1;
+    
+    aNode.physicsBody = border;
+//    self.physicsBody = border;
     [self runAction:[SKAction waitForDuration:1] completion:^{
         self.didInitContent = YES;
         [self updateState];
@@ -61,12 +74,72 @@ typedef NS_ENUM(NSInteger, PL1FastFoodGameState) {
     }
 }
 
+#pragma mark - Touch Handling
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    switch (self.state) {
+        case PL1FastFoodGameStateReadyToThrow:{
+            UITouch *anyTouch = [touches anyObject];
+            CGPoint touchCoordinate = [anyTouch locationInNode:self];
+            //Проверим попали ли мы в наш обьект для метания
+            if ([self nodeAtPoint:touchCoordinate] == self.nodeToThrow){
+                self.startDragPosition = touchCoordinate;
+                self.state = PL1FastFoodGameStateDragging;
+            }
+        }break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    switch (self.state) {
+        case PL1FastFoodGameStateDragging:{
+            UITouch *anyTouch = [touches anyObject];
+            CGPoint touchPosition = [anyTouch locationInNode:self];
+            self.nodeToThrow.position = touchPosition;
+        }break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    switch (self.state) {
+        case PL1FastFoodGameStateDragging:{
+            CGVector result = CGVectorMake(self.startDragPosition.x - self.nodeToThrow.position.x ,self.startDragPosition.y - self.nodeToThrow.position.y);
+            self.state = PL1FastFoodGameStateThrowing;
+            [self throwNode:self.nodeToThrow withDirection:result];
+        }
+        break;
+            
+        default:
+            break;
+    }
+}
+
+
 #pragma mark - Game Mechanics
+
+- (void)throwNode:(SKNode *)aNode withDirection:(CGVector)direction
+{
+    self.nodeToThrow.physicsBody = self.bodyToThrow;
+    CGFloat multilplyer = 0.4;
+    direction = CGVectorMake(direction.dx * multilplyer, direction.dy * multilplyer);
+    [self.nodeToThrow.physicsBody applyImpulse:direction];
+}
 
 - (void)putNodeToBallPosition:(SKNode *)aNode
 {
     self.nodeToThrow  = aNode;
     self.bodyToThrow  = aNode.physicsBody;
+
+    NSParameterAssert(self.bodyToThrow);
     aNode.physicsBody = nil;
     [aNode runAction:[SKAction moveTo:CGPointMake(150, 200) duration:0.25] completion:^{
         self.state = PL1FastFoodGameStateReadyToThrow;
